9214e3e833aefe9a704b6f9e20204d0a2859428a
CrossKain <densweetfit@gmail.com>
2024-05-16 16:42:45 +0500

    initial commit

--- src/components/shared/modalBox/index.tsx
+++ src/components/shared/modalBox/index.tsx
@@ -4,7 +4,7 @@ const ModalBox = () => {
   return (
     
     <div className='bg-white p-8 w-360px min-h-48 rounded-lg flex flex-col justify-between shadow-md'>
-      <h2>Заголовок</h2>
+      <h2>Заголовок </h2>
       <input placeholder='введите имя'/>
       <input placeholder='введите имя'/>
       <button>Кнопка</button>
