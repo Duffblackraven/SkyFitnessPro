d4ab2f8f31b6c9da7f98ab6ee05b9b95719e93ea
Viktor Pirogov <Blade-85@inbox.ru>
2024-06-06 11:28:43 +0300

    Регистрация:обработка ошибок полей

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,87 +1,20 @@
-
 import { signUp } from "@/api/auth";
 import Button from "@/components/shared/button";
 import Input from "@/components/shared/input/input";
 import Logo from "@/components/shared/logo";
+
+import SignUpForm from "@/components/signupForm";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import React, { useState } from "react";
 
 const SignUpPage = () => {
-  // const formFields = {
-  //   email: "",
-  //   password: "",
-  //   password_repeat: ""
-  // };
-
-  // const [formData, setFormData] = useState(formFields);
-
-  // function handleChange(e: any) {
-  //   const { name, value } = e.target;
-
-  //   setFormData({
-  //     ...formData,
-  //     [name]: value,
-  //   });
-  // }
-  // const signIn = () => {};
-  const handleSignup = async (data: FormData) => {
-    "use server"
-    let error = null
-    try {
-      const { password, email } = Object.fromEntries(data) as {password: string, email: string}
-      
-      const response = await signUp({ password, email })
-      if (!password || !email) {
-        return
-      }
-    } catch (err) {
-      if(err instanceof Error)
-      error = err.message
-    } finally {
-      if (!error) {
-        { redirect("/signin") }
-      }
-    }
-  };
   return (
     <section className="shadow-base rounded flex flex-col items-center p-[30px] w-[360px] m-auto mt-[15%] text-black">
       <Logo />
-      <div className="mt-12 mb-[34px] grid gap-y-2.5 w-full text-black">
-        <form className="grid gap-y-2.5" action={handleSignup}>
-          <Input
-            type={"email"}
-            name={"email"}
-            placeholder={"Эл. почта"}
-
-          />
-          <Input
-            type={"password"}
-            name={"password"}
-            placeholder={"Пароль"}
-
-          />
-          <Input
-            type={"password"}
-            name={"password_repeat"}
-            placeholder={"Повторите пароль"}
-
-          />
-
-          <div className="flex  mt-[24px] flex-col gap-2.5 w-full text-black">
-          <Button type={"submit"} green={true}>
-          Зарегистрироваться
-            </Button>
-            <Link
-              className="w-full text-center text-sm border py-4 px-[26px] rounded border-black hover:bg-light-grey active:bg-dark-grey"
-              href="/signin"
-            >
-              Войти
-            </Link>
-          </div>
-        </form>
-      </div>
-    </section >
+      <SignUpForm/>
+      <div className="mt-12 mb-[34px] grid gap-y-2.5 w-full text-black"></div>
+    </section>
   );
 };
 
---
+++ src/components/signupForm/index.tsx
@@ -0,0 +1,37 @@
+"use client";
+import React from "react";
+import Button from "../shared/button";
+import Input from "../shared/input/input";
+import Link from "next/link";
+import { useFormState } from "react-dom";
+import { handleSignup } from "@/actions";
+
+export const SignUpForm = () => {
+  const [state, formAction] = useFormState( handleSignup, { message: "" });
+  return (
+    <form className="grid gap-y-2.5" action={formAction}>
+      <Input type={"email"} name={"email"} placeholder={"Эл. почта"} />
+      <Input type={"password"} name={"password"} placeholder={"Пароль"} />
+      <Input
+        type={"password"}
+        name={"password_repeat"}
+        placeholder={"Повторите пароль"}
+      />
+
+      <div className="flex  mt-[24px] flex-col gap-2.5 w-full text-black">
+        <p className="text-red-600 text-center">{state?.message}</p>
+        <Button type={"submit"} green={true}>
+          Зарегистрироваться
+        </Button>
+        <Link
+          className="w-full text-center text-sm border py-4 px-[26px] rounded border-black hover:bg-light-grey active:bg-dark-grey"
+          href="/signin"
+        >
+          Войти
+        </Link>
+      </div>
+    </form>
+  );
+};
+
+export default SignUpForm;
